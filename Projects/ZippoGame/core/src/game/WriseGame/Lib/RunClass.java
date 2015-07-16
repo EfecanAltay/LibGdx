dcaa4c269c9d031be1d6a661848b8072916f72b2
WriseSoftware <efecan95.gs@gmail.com>
2015-07-16 12:44:56 +0300

    I've added Button and Action

--- Projects/ZippoGame/core/src/game/WriseGame/Lib/RunClass.java
+++ Projects/ZippoGame/core/src/game/WriseGame/Lib/RunClass.java
@@ -1,27 +1,65 @@
 package game.WriseGame.Lib;
 
+import WriseGame.StandartLib.Button;
+import WriseGame.StandartLib.Clickable;
+import WriseGame.StandartLib.Image;
+import WriseGame.StandartLib.Label;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+
 public class RunClass extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Image image ;
+	Button btnPlay ;
+	Button btnQuit ;
+	Label text ;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		btnPlay = new Button();
+		btnPlay.setHeightIWight(150, 50);
+		btnPlay.setPosition(Gdx.graphics.getWidth()/2 - btnPlay.getWight()/2,Gdx.graphics.getHeight()/2- btnPlay.getHeight()/2);
+		text = new Label("play");
+		text.setPosition(btnPlay.getPosition().x + btnPlay.getWight()/2 - 10 ,btnPlay.getPosition().y + btnPlay.getHeight()/2 +8);
+		
+		btnQuit = new Button();
+		btnQuit.setHeightIWight(150, 50);
+		btnQuit.setPosition(btnPlay.getPosition().x,btnPlay.getPosition().y - btnPlay.getHeight() - 10);
+		
+		
+		image = new Image("badlogic.jpg");
+		
+		btnPlay.setOnClick(new Clickable(btnPlay) {
+			
+			@Override
+			public void OnClick() {
+				// TODO Auto-generated method stub
+				image.Transform(1, 1);
+				
+			}
+		});
+	
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		batch.begin();
-		batch.draw(img, 0, 0);
+		
+		image.Show(batch);
+		btnPlay.Show(batch);
+		btnQuit.Show(batch);
+		text.Show(batch);
+		
+		
 		batch.end();
 	}
 }
