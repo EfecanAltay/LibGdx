107839972f82ecac679b5c18027e8709bb48ec56
WriseSoftware <efecan95.gs@gmail.com>
2015-07-16 12:09:42 +0300

    I've added a project

---
+++ Projects/ZippoGame/desktop/src/game/WriseGame/Lib/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package game.WriseGame.Lib.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import game.WriseGame.Lib.RunClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RunClass(), config);
+	}
+}
